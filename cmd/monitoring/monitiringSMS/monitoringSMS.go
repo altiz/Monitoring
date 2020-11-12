85a3f95c2f6467578a85d8de8f4c735202a77097
altiz <you@example.com>
2020-11-12 20:36:03 +0300

    20201112

---
+++ cmd/monitoring/monitiringSMS/monitoringSMS.go
@@ -0,0 +1,17 @@
+package monitiringSMS
+
+import (
+	mon "Monitoring/cmd/monitoring/common"
+	"errors"
+	"fmt"
+)
+
+func Run(debug bool) (int, error) {
+	const host string = "192.168.143.208"
+
+	_, err := mon.Ping264(host)
+	if err != nil {
+		return 0, errors.New(fmt.Sprint("Destination Host Unreachable - " + host))
+	}
+	return 1, nil
+}
