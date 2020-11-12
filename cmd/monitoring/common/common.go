332cc859c525e3d24da0e20f1f3c95e54156facd
altiz <you@example.com>
2020-11-12 20:15:16 +0300

    20201112

--- cmd/monitoring/common/common.go
+++ cmd/monitoring/common/common.go
@@ -21,3 +21,18 @@ func Ping(IP string) (models.TData_resp, error) {
 	resp.RunTime = time.Now().UnixNano() - beginTime
 	return resp, nil
 }
+
+func Ping264(IP string) (models.TData_resp, error) {
+	var resp models.TData_resp
+	Command := fmt.Sprintf("ping -c 1 -s 256 " + IP + " > /dev/null && echo 1 || echo 0")
+	beginTime := time.Now().UnixNano()
+	output, err := exec.Command("/bin/sh", "-c", Command).Output()
+	if err != nil {
+		resp.Status = "0"
+		resp.RunTime = time.Now().UnixNano() - beginTime
+		return resp, err
+	}
+	resp.Status = string(output[0])
+	resp.RunTime = time.Now().UnixNano() - beginTime
+	return resp, nil
+}
